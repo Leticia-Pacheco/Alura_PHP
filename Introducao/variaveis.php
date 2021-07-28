f8be73b0d2a8069b6a5af1015fbad00588563df1
Letícia Pacheco dos Santos <le.pacheco0310@hotmail.com>
2021-07-28 09:36:54 -0300

    Introdução ao PHP

---
+++ Introducao/variaveis.php
@@ -0,0 +1,18 @@
+<?php
+
+// Podemos declarar a variável na hora de executá-la.
+
+// Para declarar um texto (String), devemos colocá-lo dentro de aspas.
+$nome = "Letícia Pacheco";
+
+// Para declararmos um número (int, double ou float) apenas colocamos ele, sem aspas nem nada.
+$idade = 17;
+
+
+// Mesmo com o valor definido, é possível alterá-la depois, pois é uma VARIÁVEL.
+$idade = 18;
+
+// o echo serve para exibir o comando na tela.
+echo $nome;
+
+// Quando alteramos a variável, o echo sempre chamará o último valor definido.
